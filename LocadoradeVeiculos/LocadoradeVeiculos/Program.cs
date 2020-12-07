e05bd624c67b342c5d190be18c30ad510c6c994b
Guilherme Hernandes Monteiro <gui-h-m@outlook.com>
2020-12-06 22:35:38 -0300

    Test
    Test

--- LocadoradeVeiculos/LocadoradeVeiculos/Program.cs
+++ LocadoradeVeiculos/LocadoradeVeiculos/Program.cs
@@ -3,9 +3,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using LocadoradeVeiculos.Models;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace LocadoradeVeiculos
 {
@@ -13,7 +13,10 @@ namespace LocadoradeVeiculos
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //CreateHostBuilder(args).Build().Run();
+            var cliente = new Cliente () { NomeCliente = "João", CPF = "123.543.782-54" };
+            
+            Console.WriteLine(JsonSerializer.Serialize<Cliente>(cliente));
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
